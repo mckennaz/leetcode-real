535561b8affd202dc8c1deef0c5f9f34209921d8
McKenna <133259356+mckennaz@users.noreply.github.com>
2024-04-11 14:35:44 -0400

    Create 1207. Unique Number of Occurrences.js

---
+++ 1207. Unique Number of Occurrences.js
@@ -0,0 +1,25 @@
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    let map = new Map();
+    let x=0;
+    let res = new Array();
+    for(let i=0; i<arr.length; i++) {
+        if(!map.has(arr[i])) {
+            map.set(arr[i], x);
+            res[x] = 1;
+            x++;
+        } else {
+            res[map.get(arr[i])] +=1;
+        }
+    }
+    let check = [];
+    check.splice(0, 0, ...new Set(res));
+    console.log(res);
+    console.log(check);
+    return check.join('') == res.join('');
+};
